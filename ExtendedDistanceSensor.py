c13a44512f5407454ba6f25a65646d91a67f3ca2
Sam <samueljsg@gmail.com>
2020-06-01 23:24:41 -0600

    Got callbacks working for distance

--- ExtendedDistanceSensor.py
+++ ExtendedDistanceSensor.py
@@ -10,37 +10,6 @@ class ExtendedDistanceSensor(DistanceSensor, object):
         DistanceSensor.__init__(self, echo=echo, trigger=trigger)
         self._observers = []
 
-    @property
-    def distance(self):
-        """
-        Returns the current distance measured by the sensor in meters. Note
-        that this property will have a value between 0 and
-        :attr:`max_distance`.
-        """
-        return self.value * self._max_distance
-
-    @distance.setter
-    def distance(self, new_value):
-        self.distance = new_value
-        for callback in self._observers:
-            callback(self.distance)
-
-    @property
-    def value(self):
-        """
-        Returns a value between 0, indicating the reflector is either touching
-        the sensor or is sufficiently near that the sensor can't tell the
-        difference, and 1, indicating the reflector is at or beyond the
-        specified *max_distance*.
-        """
-        return super(ExtendedDistanceSensor, self).value
-
-    @DistanceSensor.value.setter
-    def value(self, new_value):
-        self.value = new_value
-        for callback in self._observers:
-            callback(self.value)
-
     def bind_to(self, callback):
         self._observers.append(callback)
 
@@ -63,3 +32,7 @@ class ExtendedDistanceSensor(DistanceSensor, object):
         distance2 = self.distance * 180
         velocity = distance2 - distance1 / time_step
         return DistanceVelocityHolder(distance2 - distance1 / 2, velocity)
+
+    def _read(self):
+        for callback in self._observers:
+            callback(super(ExtendedDistanceSensor,self)._read())
